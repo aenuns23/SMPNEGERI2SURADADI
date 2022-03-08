50cdbfed61ace68472622ef7135b4b7a878f315e
aenuns23 <aenunsafitri6@gmail.com>
2022-03-08 14:35:38 +0700

    spendura

--- app/src/main/java/com/example/smpnegeri2suradadi/MainActivity.java
+++ app/src/main/java/com/example/smpnegeri2suradadi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.smpnegeri2suradadi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void profil(View view) {
+        Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, GuruActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, FasilitasActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(MainActivity.this, PerpusActivity.class);
+        startActivity(intent);
+    }
+
+    public void eskul(View view) {
+        Intent intent = new Intent(MainActivity.this, EkskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void prestasi(View view) {
+        Intent intent = new Intent(MainActivity.this, PrestasiActivity.class);
+        startActivity(intent);
     }
 }
